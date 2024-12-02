975b19a86ed4fc2b690c9b397cf28d1d1604a3a6
Rusinov Yury <y.rusinov@kpda.ru>
2024-12-02 12:02:00 +0300

    Комментарии

--- blind_data.go
+++ blind_data.go
@@ -1,15 +1,15 @@
 package client
 
 type RepBlinds struct {
-	Ready      bool //true если прочитаны
-	Conflicts  Conflicts
-	DefineNaps DefineNaps
-	DefPhases  DefPhases
-	RPU        OnePlan
-	Plans      Plans
-	Year       Year
-	Weeks      Weeks
-	Days       Days
+	Ready      bool       //true если прочитаны
+	Conflicts  Conflicts  //Таблица конфликтов
+	DefineNaps DefineNaps //Описание направлений
+	DefPhases  DefPhases  //Описание фаз
+	RPU        OnePlan    //Резервный план управления (который работает всегда когда есть ошибки в лругих планах)
+	Plans      Plans      //Описание планов управления
+	Year       Year       //Годовая карта
+	Weeks      Weeks      //Недельные карты
+	Days       Days       //Дневные карты
 	Status     [3]int
 	SerialNum  [2]uint16
 }
@@ -18,26 +18,26 @@ type Conflicts struct {
 	Conflicts map[int]LineConflict
 }
 type LineConflict struct {
-	Number int
-	Line   []bool
+	Number int    // Номер направления
+	Line   []bool // Маска конфликтов (true если с этим направлением конфликт)
 }
 type DefineNaps struct {
 	DefineNaps map[int]DefNap
 }
 type DefNap struct {
 	Number      int
-	Type        int // 0 -не назначен 1 транспортный 2 пешеход 3 стрелка
-	FlashGreen  int
-	YellowFlash int
-	Red         int
-	RedYellow   int
-	Keys        []bool
-	Tiob        int // 0 выключен 1 постоянный 3 вызывной
+	Type        int    // 0 -не назначен 1 транспортный 2 пешеход 3 стрелка
+	FlashGreen  int    // время мигания зеленым
+	YellowFlash int    // время мигания желтым
+	Red         int    // время красного
+	RedYellow   int    // время красно-желтого
+	Keys        []bool // Ключи
+	Tiob        int    // 0 выключен 1 постоянный 3 вызывной
 }
 type DefPhase struct {
-	Number int
-	Tmin   int
-	Naps   []bool
+	Number int    //Номер фазы
+	Tmin   int    // Минимальное время фазы
+	Naps   []bool // Направления задействованные в фазе
 }
 type DefPhases struct {
 	DefPhases map[int]DefPhase
@@ -46,11 +46,11 @@ type Plans struct {
 	Plans map[int]OnePlan
 }
 type OnePlan struct {
-	Number int
-	Type   int //0 локальный план 1 координация
-	Tcycle int
-	Shift  int
-	Lines  []Line
+	Number int    // номер фазы
+	Type   int    //0 локальный план 1 координация
+	Tcycle int    //Время цикла
+	Shift  int    //Cдвиг начала цикла для типа 1
+	Lines  []Line // Лписание стадий плана
 }
 type Line struct {
 	// SIMPLE = 0, 		//0 простая фаза
@@ -63,22 +63,22 @@ type Line struct {
 	// SUB_TVP12,		//7 замещающая вызывная фаза 1 и 2
 	Type  int
 	Phase int //Номер фазы
-	Start int
-	Stop  int
+	Start int //время начала фазы от старта цикла
+	Stop  int //время завершения фазы
 }
 type Year struct {
 	Year map[int]Month
 }
 type Month struct {
 	Number int
-	Days   []int
+	Days   []int // Номера недельной карта на каждый день
 }
 type Weeks struct {
 	Weeks map[int]Week
 }
 type Week struct {
 	Number int
-	Days   []int
+	Days   []int //Номера суточных карт на каждый день недели
 }
 type Days struct {
 	Days map[int]DayPlan
@@ -89,7 +89,7 @@ type DayPlan struct {
 	Nplans []Nplan
 }
 type Nplan struct {
-	Start int
-	Stop  int
-	Plan  int
+	Start int //Время начала дествия плана (минуты от начала суток)
+	Stop  int //Время окончания действия  плана
+	Plan  int //Номер плана на этот период
 }
